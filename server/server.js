9640f354d28bf6a8cbee580067dc6be6ba44a7ca
Brian Pachucki <bjpachucki@gmail.com>
2020-05-13 18:15:08 -0500

    updated require statement on server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('./node_modules/express');
+const express = require('express');
 const app = express();
 const path = require('path')
 const publicPath = path.join(__dirname, '..', 'public')
